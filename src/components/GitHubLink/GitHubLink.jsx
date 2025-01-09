3a9ec3ca5ac286bcb6603aa121346108b0dbaac1
xlongDev <14770066+xlongdev@user.noreply.gitee.com>
2025-01-09 15:22:51 +0800

    修复giuhub地址

--- src/components/GitHubLink/GitHubLink.jsx
+++ src/components/GitHubLink/GitHubLink.jsx
@@ -3,7 +3,7 @@ import { FaGithub } from 'react-icons/fa';
 const GitHubLink = () => {
   return (
     <a
-      href="https://github.com/xlongDev/epub-zh-transform.git"
+      href="https://github.com/xlongDev/epub-zh-convert"
       target="_blank"
       rel="noopener noreferrer"
       className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
